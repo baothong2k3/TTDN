1ee5479b2da7608ccf96ae88abf2058746e956cb
Đặng Bảo Thông <baothong.work@gmail.com>
2025-11-21 07:59:23 +0700

    fix

--- monitoring_service/src/main/java/fit/monitoring_service/MonitoringServiceApplication.java
+++ monitoring_service/src/main/java/fit/monitoring_service/MonitoringServiceApplication.java
@@ -2,15 +2,10 @@ package fit.monitoring_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
-
-@SpringBootApplication
-=======
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
->>>>>>> 49ed5c534f710882895c9558d4a513c4027a03ae
 public class MonitoringServiceApplication {
 
     public static void main(String[] args) {
